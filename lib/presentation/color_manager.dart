2d3a8118c8cc45a1512e40d5beba282296f2d71a
osamadeef <osama.eljooker@gmail.com>
2022-04-03 08:08:57 +0200

    add Theme And Styles

--- lib/presentation/color_manager.dart
+++ lib/presentation/color_manager.dart
@@ -6,6 +6,13 @@ class ColorManager{
   static Color grey = const Color(0xff737477);
   static Color liteGrey = const Color(0xff9E9E9E);
 
+  static Color darkPrimary = const Color(0xffd17d11);
+  static Color lightPrimary = const Color(0xccd17d11); // Color With 80% Opacity
+  static Color grey1 = const Color(0xff707070);
+  static Color grey2 = const Color(0xff797979);
+  static Color white = const Color(0xffFFFFFF);
+  static Color error = const Color(0xffe61f34); // Red Color
 
-  // static Color liteGrey = "#304654";
+
+
 }
---
+++ lib/presentation/theme_manager.dart
@@ -0,0 +1,121 @@
+import 'package:advanced_flutter/presentation/color_manager.dart';
+import 'package:advanced_flutter/presentation/font_manager.dart';
+import 'package:advanced_flutter/presentation/styles_manager.dart';
+import 'package:advanced_flutter/presentation/values_manager.dart';
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    /// Main Colors
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.lightPrimary,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    splashColor: ColorManager.lightPrimary,
+    // Ripple Effect Color
+
+    /// CardView Theme
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
+
+    /// App Bar Theme
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.lightPrimary,
+      titleTextStyle:
+          getRegularStyle(fontSize: FontSize.s16, color: ColorManager.white),
+    ),
+
+    /// Button Theme
+    buttonTheme: ButtonThemeData(
+      shape: const StadiumBorder(),
+      disabledColor: ColorManager.grey1,
+      buttonColor: ColorManager.primary,
+      splashColor: ColorManager.lightPrimary,
+    ),
+
+    // Elevated Button Theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle:
+            getRegularStyle(fontSize: FontSize.s17, color: ColorManager.white),
+        primary: ColorManager.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppSize.s12),
+        ),
+      ),
+    ),
+
+    /// Text Theme
+    textTheme: TextTheme(
+      headline1: getSemiBoldStyle(
+          color: ColorManager.darkGrey, fontSize: FontSize.s16),
+      subtitle1:
+          getMediumStyle(color: ColorManager.liteGrey, fontSize: FontSize.s14),
+      caption: getRegularStyle(color: ColorManager.grey1),
+      bodyText1: getRegularStyle(color: ColorManager.grey),
+    ),
+
+    /// Input Decoration Theme (text form field)
+    inputDecorationTheme: InputDecorationTheme(
+      /// Content padding
+      contentPadding: const EdgeInsets.all(AppPadding.p8),
+
+      ///Hint Style
+      hintStyle:
+          getRegularStyle(color: ColorManager.grey, fontSize: FontSize.s14),
+
+      /// Label Style
+      labelStyle:
+          getMediumStyle(color: ColorManager.grey, fontSize: FontSize.s14),
+
+      /// Error Style
+      errorStyle: getRegularStyle(color: ColorManager.error),
+
+      /// Enabled Border Style (default)
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.grey,
+          width: AppSize.s1_5,
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8),
+        ),
+      ),
+
+      /// Focused Border Style
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.primary,
+          width: AppSize.s1_5,
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8),
+        ),
+      ),
+
+      /// Error Border Style
+      errorBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.error,
+          width: AppSize.s1_5,
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8),
+        ),
+      ),
+
+      /// FocusedError Border Style
+      focusedErrorBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.primary,
+          width: AppSize.s1_5,
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8),
+        ),
+      ),
+    ),
+  );
+}
